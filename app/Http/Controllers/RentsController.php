3961d7a8d36ad2c8eefd77347884ccc017b67b3a
Tarcisio Souto <tss.labsi@gmail.com>
2021-08-29 17:28:51 -0300

    migrations_tabelas_projeto

---
+++ app/Http/Controllers/RentsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Rents;
+use Illuminate\Http\Request;
+
+class RentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Rents  $rents
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Rents $rents)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Rents  $rents
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Rents $rents)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Rents  $rents
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Rents $rents)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Rents  $rents
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Rents $rents)
+    {
+        //
+    }
+}
